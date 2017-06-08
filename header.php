6a496a82bbf792fd83a36d2b970241bfbce21f64
stevendev <stevendotpulido@gmail.com>
2017-06-07 20:13:39 -0700

    updated header with fadeIn animation

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	<?php do_action( 'before' ); ?>
 
 
-	<nav class="navbar navbar-default navbar-static-top">
+	<nav class="navbar navbar-default navbar-static-top animated fadeIn">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
